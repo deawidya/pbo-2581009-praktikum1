c1e92050df3f8e7c2b8fe2f9046874fb37f0c7e4
Dea Widya Ningsih <2581009@unai.edu>
2026-09-04 13:00:38 +0700

    menambahkan asal daerah

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Biodata {
         String nama = "Dea Widya Ningsih";
         String nim = "2581009";
         String prodi = "Teknik Informatika";
+        String asal_daerah = "Sumatera Selatan";
         System.out.println("Nama : " + nama);
         System.out.println("NIM  : " + nim);
     }
