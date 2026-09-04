43be02e3647eda07ee203acabeb6b657b9f0a61b
Dea Widya Ningsih <2581009@unai.edu>
2026-09-04 12:57:57 +0700

    menambahkan program studi

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Biodata {
     public static void main(String[] args) {
         String nama = "Dea Widya Ningsih";
         String nim = "2581009";
+        String prodi = "Teknik Informatika";
         System.out.println("Nama : " + nama);
         System.out.println("NIM  : " + nim);
     }
