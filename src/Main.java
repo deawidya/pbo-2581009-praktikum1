0fd244f4174c0b4db9fa5c7fcec2bd26c7391aae
Dea Widya Ningsih <2581009@unai.edu>
2026-09-04 13:04:03 +0700

    menambahkan alasan memilih prodi

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Biodata {
         String nim = "2581009";
         String prodi = "Teknik Informatika";
         String asal_daerah = "Sumatera Selatan";
+        String alasan_memilih_prodi = "Saya memeilih Teknik Informatika karena saya berminat dengan dunai teknologi yang semakin berkambang";
         System.out.println("Nama : " + nama);
         System.out.println("NIM  : " + nim);
     }
