fb5db8bdb2b951a89720071b3e93ce2cfd129033
Dea Widya Ningsih <2581009@unai.edu>
2026-09-04 12:50:06 +0700

    menambahkan nama

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Biodata {
+    public static void main(String[] args) {
+        String nama = "Dea Widya Ningsih";
+        String nim  = "2581009";
+        System.out.println("Nama : " + nama);
+        System.out.println("NIM  : " + nim);
+    }
+}
